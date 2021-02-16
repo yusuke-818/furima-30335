c113bbba474d8d54b89ce20b0efc473bb131c952
yusuke-818 <pokko.pen@icloud.com>
2021-02-16 21:52:48 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
     validates :first_name
     validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters' } do
     validates :first_name_reading
     validates :last_name_reading
   end
