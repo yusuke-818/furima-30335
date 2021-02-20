751d37459b47bf9e38a540b2c5928c1a2d1e9e6b
yusuke-818 <pokko.pen@icloud.com>
2021-02-20 18:10:18 +0900

    Active Storageの導入

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -3,5 +3,11 @@ class FurimasController < ApplicationController
   def new
     @furima = Furima.new
   end
+  
+  private
+
+  def furima_params
+    params.require(:furima).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/furima.rb
+++ app/models/furima.rb
@@ -1,4 +1,5 @@
 class Furima < ApplicationRecord
   has_one :order
   belongs_to :user
+  has_one_attached :image
 end
