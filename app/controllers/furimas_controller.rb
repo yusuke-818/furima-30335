694a5c0a1d46f0b9ab0f1cce9591822c10c48217
yusuke-818 <pokko.pen@icloud.com>
2021-03-07 20:33:59 +0900

    商品情報編集機能の実装

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,5 +1,7 @@
 class FurimasController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_furima, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @furimas = Furima.all.order('created_at DESC')
@@ -19,13 +21,36 @@ class FurimasController < ApplicationController
   end
 
   def show
-    @furima = Furima.find(params[:id])
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+    if @furima.update(furima_params)
+      redirect_to furima_path
+    else
+      render :edit
+    end
   end
 
   private
 
+  def set_furima
+    @furima = Furima.find(params[:id])
+  end
+
   def furima_params
     params.require(:furima).permit(:image, :item, :info, :category_id, :states_id, :shipping_id, :prefecture_id,
                                    :shipping_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  #<% if current_user.id == @furima.user_id %>
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @furima.user
   end
 end
