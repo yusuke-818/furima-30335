c0efdde4be8c9f116206843004a7c993d3feb1f9
yusuke-818 <pokko.pen@icloud.com>
2021-03-13 10:38:41 +0900

    削除機能実装　修正２

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,5 +1,5 @@
 class FurimasController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_furima, only: [:show, :edit, :update, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
