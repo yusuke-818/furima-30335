78063145ec2ceb9845919fe47cee076f9803220e
yusuke-818 <pokko.pen@icloud.com>
2021-03-24 19:58:25 +0900

    model　データをテーブルに保存する処理

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -9,9 +9,23 @@ class UserOrder
     validates :phone_number, format: { with: /\A[0-9]+\z/, message: "Phone number Input only number" }
   end
   validates :prefecture, numericality: { other_than: 0, message: "Prefecture Select" }
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters' } do
+    validates :first_name_reading
+    validates :last_name_reading
+  end
   
   def save
-    #furima.create()
-    addresses.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, phone_number: phone_number, user_id: user_id)
+    user = User.create(nickname: nickname, last_name: last_name, first_name: first_name)
+    furima = Furima.create(item: item)
+    addresses.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, phone_number: phone_number, user_id: user.id, furima_id: furima.id)
   end
 end
